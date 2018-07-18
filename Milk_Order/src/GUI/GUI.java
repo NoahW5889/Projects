e506524914bbee55c2f077763605faf004e49412
Noah <35859425+NoahW5889@users.noreply.github.com>
2018-07-18 13:16:09 -0400

    Seach Function Added

--- Milk_Order/src/GUI/GUI.java
+++ Milk_Order/src/GUI/GUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -11,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
 import code.Driver;
@@ -20,38 +22,100 @@ public class GUI {
 
 	private Driver _windowHolder;
 	private JPanel _mainPanel;
-	private JPanel _panel;
+	private JPanel _scrollPanel;
 	private JPanel _productPanel;
 	private JPanel _codePanel;
+	private JPanel _textPanel;
+	private JPanel _searchPanel;
+	private JTextField search;
+	private ArrayList<String> prodList;
+	private ArrayList<String> code;
 	
 	public GUI(OrderSheet os, JPanel mp, Driver driver) {
 	
 		_windowHolder = driver;
 		_mainPanel = mp;
-		_mainPanel.setLayout(new BoxLayout(_mainPanel, BoxLayout.X_AXIS));
+		_mainPanel.setLayout(new BoxLayout(_mainPanel, BoxLayout.Y_AXIS));
 		_productPanel = new JPanel();
 		_productPanel.setLayout(new GridLayout(os.getProducts().size()+3,9));
 		_codePanel = new JPanel();
 		_codePanel.setLayout(new GridLayout(os.getCodes().size()+3,9));
+		_textPanel = new JPanel();
+		_textPanel.setLayout(new GridLayout(os.getProducts().size()+3,9));
 		
-		for(int i=0;i<os.getProducts().size();i++) {
-			JLabel add = new JLabel(os.getProducts().get(i));
-			setLabelProperties(add);
-			_productPanel.add(add);
-		}
+		JLabel upstate = new JLabel("Upstate Farms: (716) 892-3434");
+		setTitleProperties(upstate);
+		_mainPanel.add(upstate);
+		
+		JLabel title = new JLabel("WEEKLY MILK ORDER SHEET FOR W/E:");
+		setTitleProperties(title);
+		_mainPanel.add(title);
+		
+		JLabel customerNum = new JLabel("Customer Number: 110 420 461");
+		setTitleProperties(customerNum);
+		_mainPanel.add(customerNum);
+		
+		_searchPanel = new JPanel();
+		JButton searchBox = new JButton("Search:");
+		searchBox.addActionListener(new searchHandler(this));
+		setButtonProperties(searchBox);
+		_searchPanel.add(searchBox);
+		
+		search = new JTextField();
+		search.setColumns(30);
+		_searchPanel.add(search);
+		_mainPanel.add(_searchPanel);
+		
+		prodList=new ArrayList<String>();
+		code=new ArrayList<String>();
+		
 		
 		for(int i=0;i<os.getCodes().size();i++) {
 			JLabel add = new JLabel(os.getCodes().get(i));
 			setLabelProperties(add);
+			code.add(os.getCodes().get(i));
 			_codePanel.add(add);
 		}
 		
-		_panel=new JPanel();
-		_panel.setLayout(new BoxLayout(_panel, BoxLayout.X_AXIS));
-		_panel.add(_codePanel);
-		_panel.add(_productPanel);
-		JScrollPane scroll = new JScrollPane(_panel);
+		for(int i=0;i<os.getProducts().size();i++) {
+			JLabel add = new JLabel(os.getProducts().get(i));
+			setLabelProperties(add);
+			prodList.add(os.getProducts().get(i));
+			_productPanel.add(add);
+		}
+		
+		for(int i=0;i<os.getProducts().size();i++) {
+			if(os.getCodes().get(i).replaceAll("[^0-9]", "").equals("")) {
+				JLabel spacer = new JLabel(os.getCodes().get(i));
+				setLabelProperties(spacer);
+				_textPanel.add(spacer);
+			}
+			else {
+		JTextField text = new JTextField();	
+		_textPanel.add(text);
+			}
+		}
+		
+		_scrollPanel=new JPanel();
+		_scrollPanel.setLayout(new BoxLayout(_scrollPanel, BoxLayout.X_AXIS));
+		_scrollPanel.add(_codePanel);
+		_scrollPanel.add(_productPanel);
+		_scrollPanel.add(_textPanel);
+		JScrollPane scroll = new JScrollPane(_scrollPanel);
 		_mainPanel.add(scroll, BorderLayout.EAST);
+	}
+	
+	public String getSearch() {
+		return this.search.getText();
+		
+	}
+	
+	public void setTitleProperties(JLabel label) {
+		label.setFont(new Font("Courier", Font.BOLD, 28));
+		label.setBackground(Color.WHITE);
+		label.setForeground(Color.BLACK);
+		label.setOpaque(true);
+		label.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.LIGHT_GRAY));
 	}
 	
 	public void setButtonProperties(JButton button) {
@@ -77,6 +141,24 @@ public class GUI {
 		label.setOpaque(true);
 		label.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.LIGHT_GRAY));
 		}
+	}
+
+	public void search(String search2) {
+		_scrollPanel.removeAll();
+		for(int i=0;i<prodList.size();i++) {
+			if(prodList.get(i).toLowerCase().contains(search2.toLowerCase())) {
+				JLabel cod = new JLabel(code.get(i));
+				setLabelProperties(cod);
+				JLabel sir = new JLabel(prodList.get(i));
+				setLabelProperties(sir);
+				JTextField num = new JTextField();
+				num.setColumns(20);
+				_scrollPanel.add(cod);
+				_scrollPanel.add(sir);
+				_scrollPanel.add(num);
+			}
+		}
+		
 	}
 	
 }
